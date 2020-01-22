3184769f4346ae6b372b877ae9d842d863bfcfbb
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-01-22 04:57:17 +0100

    16.5 fix searchStringRedux and Containers in List/Search

--- .history/src/components/Header/Header_20200120051039.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import {NavLink, Link} from 'react-router-dom';
-import Header from './Header'
--- .history/src/components/Header/Header_20200120051123.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import {NavLink, Link} from 'react-router-dom';
-import styles from './Header.scss';
